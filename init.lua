20fe96d10d0c50729152df538efdb0a0fd488272
Yousef Elsayed <yousef4511@outlook.com>
2024-07-04 12:20:35 +0300

    Add keymaps for better indentation

--- init.lua
+++ init.lua
@@ -161,6 +161,10 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Better Indentation keymaps
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
